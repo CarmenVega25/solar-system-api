7b8cd505c506ae107e3e043195cd83108f57aa74
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-10-20 11:45:35 -0700

    created blueprint for planet and decorator

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 class Planet:
     def __init__(self, id, name, descrpiton, radius):
@@ -13,3 +13,14 @@ planet_list = [
     Planet(3, "Neptune", "Farthest planet", 15,299),
     Planet(4, "Mercury", "Closest to the sun", 1,516)
     ]
+
+planet_bp = Blueprint("planets",__name__, url_prefix="/planets")
+
+@planet_bp.route('', methods=['GET'])
+def get_all_planets():
+    result = []
+    for planet in planet_list:
+        planet_dict = {"id":planet.id, "name":planet.name,
+                        "description":planet.description, "radius":planet.radius}
+        result.append(planet_dict)
+    return jsonify(result), 200
