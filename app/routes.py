99d7b0c37f0c44390b38578156cbb1b61845ad19
Carmen Vega <cvega23@horizon.csueastbay.edu>
2022-10-20 11:29:13 -0700

    Step 3 wave 1 finished

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,8 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self, id, name, descrpiton, mass):
+        self.id = id
+        self.name = name
+        self.description = descrpiton
+        self.mass = mass
