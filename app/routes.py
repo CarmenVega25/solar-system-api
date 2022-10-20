4bb3abc935a0b6337dada16e8eb5b4818bdde470
Carmen Vega <cvega23@horizon.csueastbay.edu>
2022-10-20 11:36:47 -0700

    Finished step 4 wave 1

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,15 @@
 from flask import Blueprint
 
 class Planet:
-    def __init__(self, id, name, descrpiton, mass):
+    def __init__(self, id, name, descrpiton, radius):
         self.id = id
         self.name = name
         self.description = descrpiton
-        self.mass = mass
+        self.radius = radius
+
+planet_list = [
+    Planet(1, "Mars", "Red planet",  2,106.1),
+    Planet(2, "Saturn", "Planet with rings", 36,184),
+    Planet(3, "Neptune", "Farthest planet", 15,299),
+    Planet(4, "Mercury", "Closest to the sun", 1,516)
+    ]
