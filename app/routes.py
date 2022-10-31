baf3320b9662132fe6200cb3e44ff736ed7d5d08
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-10-31 13:16:29 -0700

    refactored get_one_planet

--- app/routes.py
+++ app/routes.py
@@ -27,29 +27,29 @@ planet_bp = Blueprint("planet_bp",__name__, url_prefix="/planet")
 #         result.append(planet_dict)
 #     return jsonify(result), 200
 
-# @planet_bp.route('/<planet_id>', methods=['GET'])
-# def get_one_planet(planet_id):
-#     try:
-#         planet_id = int(planet_id)
-    # except ValueError:
-    #     return jsonify({"msg": f"Invalid data type {planet_id}"}), 400
+@planet_bp.route('/<planet_id>', methods=['GET'])
+def get_one_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        return jsonify({"msg": f"Invalid data type {planet_id}"}), 400
     
-    # chosen_planet = None
+    chosen_planet = Planet.query.get(planet_id)
 
     # for planet in planet_list:
     #     if planet.id==planet_id:
     #         chosen_planet = planet
     
-    # if chosen_planet is None:
-    #     return jsonify({"msg": f"Could not find a planet with id: {planet_id}"}), 404
+    if chosen_planet is None:
+        return jsonify({"msg": f"Could not find a planet with id: {planet_id}"}), 404
 
-    # return_planet = {
-    #     "id": chosen_planet.id, 
-    #     "name": chosen_planet.name, 
-    #     "description": chosen_planet.description,
-    #     "radius": chosen_planet.radius
-    # }
-    # return jsonify(return_planet), 200
+    return_planet = {
+        "id": chosen_planet.id, 
+        "name": chosen_planet.name, 
+        "description": chosen_planet.description,
+        "radius": chosen_planet.radius
+    }
+    return jsonify(return_planet), 200
         
 @planet_bp.route('', methods=["POST"])
 def create_one_planet():
